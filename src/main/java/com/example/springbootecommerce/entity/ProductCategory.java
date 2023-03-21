2509b644195f33d15d290f1ff5ecbfcd3333c503
Sumit Kumar <kumarsumit8210@gmail.com>
2023-03-22 04:23:29 +0530

    initial commit

---
+++ src/main/java/com/example/springbootecommerce/entity/ProductCategory.java
@@ -0,0 +1,24 @@
+package com.example.springbootecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+@Data
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
